ab4c0e3f548864264fbe8b632defba848ac31815
0xdeenz <0xdeenz@protonmail.com>
2022-11-23 14:19:08 +0100

    add gitignore

--- week-1/vignere/target/debug/deps/vignere.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sergi\Desktop\deenz\Ebin\crypto-engineering\week-1\vignere\target\debug\deps\vignere.exe: src\main.rs
-
-C:\Users\sergi\Desktop\deenz\Ebin\crypto-engineering\week-1\vignere\target\debug\deps\vignere.d: src\main.rs
-
-src\main.rs:
--- week-1/vignere/target/debug/vignere.d
+++
@@ -1,1 +0,0 @@
-C:\Users\sergi\Desktop\deenz\Ebin\crypto-engineering\week-1\vignere\target\debug\vignere.exe: C:\Users\sergi\Desktop\deenz\Ebin\crypto-engineering\week-1\vignere\src\main.rs
